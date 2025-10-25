acf7ac9d0b63133ff2d36812c7fafd6152764d3e
Andre <andrenogueiraa@gmail.com>
2025-10-25 10:57:41 -0300

    Implement revalidation feature for development teams in page.tsx. Add a button to trigger revalidation when no teams are found, enhancing user experience. Improve error handling for loading states and ensure proper rendering of development teams and their associated projects.

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,9 @@ import { getColorClassName } from "@/enums/colors";
 import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
+import { revalidatePath } from "next/cache";
+import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Visão Geral",
@@ -32,11 +35,19 @@ export default async function Server() {
           <DevTeams />
         </Suspense>
 
-        <section className="prose">
+        <section className="prose space-y-4">
           <h2>Backlog de Projetos</h2>
           <Suspense fallback={<div>Carregando Backlog de Projetos...</div>}>
             <ProjectsWithNoTeam />
           </Suspense>
+        </section>
+
+        <section>
+          <form action={revalidateDevTeams} className="flex justify-end">
+            <Button type="submit" className="w-fit" variant={"link"}>
+              .
+            </Button>
+          </form>
         </section>
       </PgContent>
     </Pg>
@@ -60,80 +71,100 @@ async function getDevTeams() {
   });
 }
 
+async function revalidateDevTeams() {
+  "use server";
+  revalidatePath("/");
+  redirect("/");
+}
+
 async function DevTeams() {
   const devTeams = await getDevTeams();
 
-  return (
-    <section className="space-y-8">
-      <div className="flex gap-6 text-center pt-6">
-        <div className="min-w-48 font-semibold"></div>
-        <div className="w-full max-w-sm font-semibold">Projeto Atual</div>
-        <div className="w-full max-w-sm font-semibold">Próximo</div>
-      </div>
+  if (!devTeams) {
+    return <div>Erro ao carregar times de desenvolvimento</div>;
+  }
 
-      {devTeams.map((devTeam) => (
-        <div key={devTeam.name} className="flex gap-6">
-          <Link
-            href={`/dev-teams/${devTeam.id}`}
-            className="flex flex-col items-center justify-center min-w-48 max-w-48"
-          >
-            <Image
-              src={devTeam.imageUrl ?? ""}
-              alt={devTeam.name ?? ""}
-              width={100}
-              height={100}
-              className="rounded-full w-20 h-20"
-            />
-            <h1 className="font-semibold mt-2">{devTeam.name}</h1>
-            <p className="leading-4 text-sm text-muted-foreground">
-              {devTeam.description ?? ""}
-            </p>
-          </Link>
+  if (devTeams.length === 0) {
+    return <div>Nenhum time de desenvolvimento encontrado.</div>;
+  }
 
-          <div className="flex gap-6">
-            {devTeam.projects &&
-              devTeam.projects.map((project, index) => (
-                <Link
-                  key={project.id}
-                  href={`/projects/${project.id}`}
-                  className={cn(
-                    "block p-3 rounded-md min-w-sm max-w-sm",
-                    index > 1
-                      ? "bg-slate-100 dark:bg-slate-700"
-                      : getColorClassName(project.color)
-                  )}
-                >
-                  <h2 className="font-medium">{project.name}</h2>
-                  <p className="text-muted-foreground">{project.description}</p>
-
-                  <div>
-                    {project.sprints &&
-                      project.sprints.map((sprint, index) => (
-                        <div key={index}>
-                          <small className="text-xs">{sprint.name}</small>
-                          <Progress value={sprint.progress} />
-                          <div className="flex justify-between">
-                            <small>
-                              {new Date(
-                                sprint.startDate ?? ""
-                              ).toLocaleDateString("pt-br")}
-                            </small>
-                            <small>
-                              {new Date(
-                                sprint.finishDate ?? ""
-                              ).toLocaleDateString("pt-br")}
-                            </small>
-                          </div>
-                        </div>
-                      ))}
-                  </div>
-                </Link>
-              ))}
-          </div>
+  if (devTeams.length > 0) {
+    return (
+      <section className="space-y-8">
+        <div className="flex gap-6 text-center pt-6">
+          <div className="min-w-48 font-semibold"></div>
+          <div className="w-full max-w-sm font-semibold">Projeto Atual</div>
+          <div className="w-full max-w-sm font-semibold">Próximo</div>
         </div>
-      ))}
-    </section>
-  );
+
+        {devTeams.map((devTeam) => (
+          <div key={devTeam.name} className="flex gap-6">
+            <Link
+              href={`/dev-teams/${devTeam.id}`}
+              className="flex flex-col items-center justify-center min-w-48 max-w-48"
+            >
+              <Image
+                src={devTeam.imageUrl ?? ""}
+                alt={devTeam.name ?? ""}
+                width={100}
+                height={100}
+                className="rounded-full w-20 h-20"
+              />
+              <h1 className="font-semibold mt-2">{devTeam.name}</h1>
+              <p className="leading-4 text-sm text-muted-foreground">
+                {devTeam.description ?? ""}
+              </p>
+            </Link>
+
+            <div className="flex gap-6">
+              {devTeam.projects &&
+                devTeam.projects.map((project, index) => (
+                  <Link
+                    key={project.id}
+                    href={`/projects/${project.id}`}
+                    className={cn(
+                      "block p-3 rounded-md min-w-sm max-w-sm",
+                      index > 1
+                        ? "bg-slate-100 dark:bg-slate-700"
+                        : getColorClassName(project.color)
+                    )}
+                  >
+                    <h2 className="font-medium">{project.name}</h2>
+                    <p className="text-muted-foreground">
+                      {project.description}
+                    </p>
+
+                    <div>
+                      {project.sprints &&
+                        project.sprints.map((sprint, index) => (
+                          <div key={index}>
+                            <small className="text-xs">{sprint.name}</small>
+                            <Progress value={sprint.progress} />
+                            <div className="flex justify-between">
+                              <small>
+                                {new Date(
+                                  sprint.startDate ?? ""
+                                ).toLocaleDateString("pt-br")}
+                              </small>
+                              <small>
+                                {new Date(
+                                  sprint.finishDate ?? ""
+                                ).toLocaleDateString("pt-br")}
+                              </small>
+                            </div>
+                          </div>
+                        ))}
+                    </div>
+                  </Link>
+                ))}
+            </div>
+          </div>
+        ))}
+      </section>
+    );
+  }
+
+  return <div>Erro desconhecido</div>;
 }
 
 async function getProjectsWithNoTeam() {
@@ -152,11 +183,15 @@ async function ProjectsWithNoTeam() {
   const projectsWithNoTeam = await getProjectsWithNoTeam();
 
   if (!projectsWithNoTeam) {
-    return <div>Erro ao carregar projetos</div>;
+    return (
+      <div className="text-muted-foreground">Erro ao carregar projetos</div>
+    );
   }
 
   if (projectsWithNoTeam.length === 0) {
-    return <div>Nenhum projeto encontrado</div>;
+    return (
+      <div className="text-muted-foreground">Nenhum projeto encontrado</div>
+    );
   }
 
   if (projectsWithNoTeam.length > 0) {
